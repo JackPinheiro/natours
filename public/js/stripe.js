6adaeafd784c98c5339d740a251fcfacad05f9d4
JackPinheiro <jackfilipepinheiro@gmail.com>
2023-03-16 13:17:48 +0000

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(
+  'pk_test_51MmDOuCXFnINgzhKx34BrHH0x5P21T2lL1bhGZQrRwHPaOr7O96BSPLk5fyDYJCJ9hm33lPZN2ULvIXooLde68z200NALBB290'
+);
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    showAlert('error', err);
+  }
+};
